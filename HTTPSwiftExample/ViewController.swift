7998629f8bb760512068643a5a6099bbac1ed422
Eric Larson <eclarson@erics-macbook-pro.local>
2016-10-24 09:48:41 -0500

    updated app security authority for plain text, updated swift 3 http conventions with new error handling

--- HTTPSwiftExample/ViewController.swift
+++ HTTPSwiftExample/ViewController.swift
@@ -6,7 +6,11 @@
 //  Copyright (c) 2015 Eric Larson. All rights reserved.
 //
 
-let SERVER_URL = "http://erics-macbook-pro.local:8000"
+// This exampe is meant to be run with the python example:
+//              tornado_example.py 
+//              from the course GitHub repository: tornado_bare, branch sklearn_example
+
+let SERVER_URL = "http://erics-macbook-pro.local:8000" // change this for your server name!!!
 
 import UIKit
 
@@ -29,10 +33,9 @@ class ViewController: UIViewController, URLSessionDelegate {
         self.session = URLSession(configuration: sessionConfig,
             delegate: self,
             delegateQueue:self.operationQueue)
-
-        
     }
 
+    //MARK: Get Request
     @IBAction func sendGetRequest(_ sender: AnyObject) {
         // create a GET request and get the reponse back as NSData
         let baseURL = "\(SERVER_URL)/GetExample"
@@ -42,9 +45,8 @@ class ViewController: UIViewController, URLSessionDelegate {
         let request: URLRequest = URLRequest(url: getUrl!)
         let dataTask : URLSessionDataTask = self.session.dataTask(with: request,
             completionHandler:{(data, response, error) in
-                
-                // TODO: handle error
-                NSLog("Response:\n%@",response)
+                // TODO: handle error!
+                print("Response:\n%@",response)
                 let strData = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
                 NSLog("\n\nData:\n%@",strData!)
         })
@@ -53,13 +55,14 @@ class ViewController: UIViewController, URLSessionDelegate {
         
     }
     
+    //MARK: Post Request, args in url
     @IBAction func sendPostRequest(_ sender: AnyObject) {
         
         let baseURL = "\(SERVER_URL)/DoPost"
         let postUrl = URL(string: "\(baseURL)")
         
         // create a custom HTTP POST request
-        var request = NSMutableURLRequest(url: postUrl!)
+        var request = URLRequest(url: postUrl!)
         
         // data to send in body of post request (style of get arguments)
         let requestBody:Data? = "arg1=\(self.floatValue)".data(using: String.Encoding.utf8, allowLossyConversion: false)
@@ -69,48 +72,67 @@ class ViewController: UIViewController, URLSessionDelegate {
         
         let postTask : URLSessionDataTask = self.session.dataTask(with: request,
             completionHandler:{(data, response, error) in
-                // TODO: handle error
-                NSLog("Response:\n%@",response)
-                var jsonError: NSError?
-                var jsonDictionary: NSDictionary = JSONSerialization.JSONObjectWithData(data, options: JSONSerialization.ReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
-                
-                NSLog("\n\nJSON Data:\n%@",jsonDictionary)
+                // TODO: handle error!
+                print("Response:\n%@",response)
+                let jsonDictionary = self.convertDataToDictionary(with: data)
+                print("\n\nJSON Data:\n%@",jsonDictionary)
         })
         
         postTask.resume() // start the task
     }
     
-    
+    //MARK: Post Request, args in request body (preferred)
     @IBAction func sendPostWithJsonInBody(_ sender: AnyObject) {
         
         let baseURL = "\(SERVER_URL)/PostWithJson"
         let postUrl = URL(string: "\(baseURL)")
         
         // create a custom HTTP POST request
-        var request = NSMutableURLRequest(url: postUrl!)
+        var request = URLRequest(url: postUrl!)
         
         // data to send in body of post request (send arguments as json)
-        var jsonError: NSError? = nil;
-        var jsonUpload:NSDictionary = ["arg":[3.2,self.floatValue*2,self.floatValue]]
-        var requestBody:Data? = JSONSerialization.dataWithJSONObject(jsonUpload,
-            options:JSONSerialization.WritingOptions.PrettyPrinted,
-            error:&jsonError);
+        let jsonUpload:NSDictionary = ["arg":[3.2,self.floatValue*2,self.floatValue]]
         
+        
+        let requestBody:Data? = self.convertDictionaryToData(with:jsonUpload)
+    
         request.httpMethod = "POST"
         request.httpBody = requestBody
         
         let postTask : URLSessionDataTask = self.session.dataTask(with: request,
-            completionHandler:{(data, response, error) in
-                NSLog("Response:\n%@",response)
-                // TODO: handle error
-                var jsonError: NSError?
-                var jsonDictionary: NSDictionary = JSONSerialization.JSONObjectWithData(data, options: JSONSerialization.ReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
-                
-                NSLog("\n\nJSON Data:\n%@",jsonDictionary)
+                        completionHandler:{(data, response, error) in
+                            print("Response:\n%@",response)
+                            let jsonDictionary = self.convertDataToDictionary(with: data)
+                            print("\n\nJSON Data:\n%@",jsonDictionary)
         })
         
         postTask.resume() // start the task
         
+    }
+    
+    //MARK: JSON Conversion Functions
+    func convertDictionaryToData(with jsonUpload:NSDictionary) -> Data?{
+        do { // try to make JSON and deal with errors using do/catch block
+            let requestBody = try JSONSerialization.data(withJSONObject: jsonUpload, options:JSONSerialization.WritingOptions.prettyPrinted)
+            return requestBody
+        } catch {
+            print("json error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func convertDataToDictionary(with data:Data?)->NSDictionary{
+        do { // try to parse JSON and deal with errors using do/catch block
+            let jsonDictionary: NSDictionary =
+                try JSONSerialization.jsonObject(with: data!,
+                                              options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            
+            return jsonDictionary
+            
+        } catch {
+            print("json error: \(error.localizedDescription)")
+            return NSDictionary() // just return empty
+        }
     }
 
 }
