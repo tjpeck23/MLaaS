a08685dcc293560a3ca6dd8cf100d25825536873
tjpeck23 <tjpeck@smu.edu>
2024-12-15 14:56:46 -0600

    Added new view controller that has the authentication thus far

---
+++ HTTPSwiftExample/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  HTTPSwiftExample
+//
+//  Created by Travis Peck on 12/15/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
