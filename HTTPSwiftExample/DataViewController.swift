70c8a693202896a31d26c0b3f503f76db3ac74d6
tjpeck23 <tjpeck@smu.edu>
2024-12-15 14:56:59 -0600

    added auth view controller

--- HTTPSwiftExample/DataViewController.swift
+++ HTTPSwiftExample/DataViewController.swift
@@ -17,56 +17,14 @@ class DataViewController: UIViewController, PredictionDelegate, AVCaptureVideoDa
     @IBOutlet weak var predictLabel: UILabel!
     
     let mlaasmodel = MlaasModel()
-    var captureSession: AVCaptureSession!
-    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     var featureImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCamera()
         // Do any additional setup after loading the view.
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
-    }
-    
-    func setupCamera() {
-        captureSession = AVCaptureSession()
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
-            print("No camera is available")
-            return
-        }
-        
-        let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice)
-        if let videoInput = videoInput, captureSession.canAddInput(videoInput) {
-            captureSession.addInput(videoInput)
-        }
-        
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-        if captureSession.canAddOutput(videoOutput) {
-            captureSession.addOutput(videoOutput)
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = view.layer.bounds
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        
-        captureSession.startRunning()
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            print("Pixel buffer not available")
-            return
-        }
-        
-        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-        let uiImage = UIImage(ciImage: ciImage)
-        
-        DispatchQueue.main.async {
-            self.mlaasmodel.uploadImageWithLabel(image: uiImage, label: "", modelType: "KNN")
-        }
     }
     
     @IBAction func sendDataButton(_ sender: Any) {
