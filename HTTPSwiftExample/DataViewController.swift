1e733ca54e6a9fb7dd57aceee7224521cb1a1168
tjpeck23 <tjpeck@smu.edu>
2024-12-16 12:48:27 -0600

    test

--- HTTPSwiftExample/DataViewController.swift
+++ HTTPSwiftExample/DataViewController.swift
@@ -22,6 +22,7 @@ class DataViewController: UIViewController, PredictionDelegate, AVCaptureVideoDa
     
     var selectedImages: [UIImage] = []
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
