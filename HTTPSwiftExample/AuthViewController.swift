84c302e291dbfdef4a652f37cad18eba6f1bbcd6
tjpeck23 <tjpeck@smu.edu>
2024-12-15 20:08:55 -0600

    Removed model type specification

--- HTTPSwiftExample/AuthViewController.swift
+++ HTTPSwiftExample/AuthViewController.swift
@@ -11,6 +11,7 @@ import AVFoundation
 
 class AuthViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
+    var receivedText = ""
     let mlaasmodel = MlaasModel()
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
